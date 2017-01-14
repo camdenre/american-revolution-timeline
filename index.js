90d32063c1e9f92a4baf4dc2c074fab9e0eb97db
Camden Reslink <camden@dashinggoods.com>
2017-01-14 16:46:03 -0500

    still trying to fix mlab

--- index.js
+++ index.js
@@ -3,27 +3,38 @@ var app = express();
 var mongoose = require('mongoose');
 var Battle = require('./models/battle');
 
-mongoose.connect('mongodb://american-revolution-timeline:dbpw1234@ds111549.mlab.com:11549/heroku_9wc8t3xm');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  var battle = new Battle({
-  	title: "Demo Battle",
-  	start_date: "1/15/2017"
-  });
+	var battle = new Battle({
+		title: "Demo Battle",
+		start_date: "1/15/2017"
+	});
 
-  battle.save(function(err) {
-  	if (err) {
-  		res.send("Success!");
-  	} else {
-  		res.send("Failure!");
-  	}
-  });
+	battle.save(function(err) {
+		if (err) {
+			res.send("Success!");
+		} else {
+			res.send("Failure!");
+		}
+	});
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+				replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+ 
+var mongodbUri = 'mongodb://american-revolution-timeline:dbpw1234@ds111549.mlab.com:11549/heroku_9wc8t3xm';
+ 
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+	app.listen(app.get('port'), function() {
+		console.log('Node app is running on port', app.get('port'));
+	});                         
 });
