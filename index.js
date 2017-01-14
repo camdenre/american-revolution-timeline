3d04298c98eb72eb276c36eec77f1bc2a5a41b51
Camden Reslink <camden@dashinggoods.com>
2017-01-14 14:09:40 -0500

    Added Express demo, port fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,4 +8,8 @@ app.use(express.static(__dirname + '/public'));
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
-})
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
