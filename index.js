32d6294233bef3ba4949198b86dac56f4ed197df
Camden Reslink <camden@dashinggoods.com>
2017-01-14 13:58:41 -0500

    Listening on port, Express demo

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
